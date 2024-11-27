8ce91db7a6140d601d566609d561120cbd9f0571
kuronokurujp <kuronokurujp@gmail.com>
2024-11-27 15:17:43 +0900

    [modify / add / del] パラメータファイルを新しいフォーマットに変更

--- SuperStar/UpdateAsset.bat
+++ SuperStar/UpdateAsset.bat
@@ -5,7 +5,7 @@ rem アセットデータを更新バッチ
 setlocal
 
 rem パラメータデータをコンバート
-Tool\GameDataMaker\20241121_180832\main.exe conv-param .\Game\Resource\Assets\Paramater .\Game\Resource\Orginal\Paramater\Game.xlsx
+Tool\GameDataMaker\20241121_180832\main.exe conv-param .\Game\Resource\Assets\Paramater .\Game\Resource\Orginal\Paramater\Game.xlsm
 
 rem アセットファイルを読み込みディレクトリに転送
 call .\HobbyEngine\Script\MoveDir.bat .\Game\Resource\Assets\ .\Bin\SuperStar\Debug\Assets
