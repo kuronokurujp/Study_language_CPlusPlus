be2795b3ca866f991d999e57cf9f1ba8ef7bfa19
kuronokurujp <kuronokurujp@gmail.com>
2024-12-06 18:03:46 +0900

    [modify] 更新アセットにテスト用パラメータを追加

--- SuperStar/UpdateAsset.bat
+++ SuperStar/UpdateAsset.bat
@@ -1,17 +1,26 @@
 @echo off
+chcp 65001
 
 rem アセットデータを更新バッチ
 
 setlocal
 
-rem パラメータデータをコンバート
+REM パラメータデータをコンバート
 Tool\GameDataMaker\20241121_180832\main.exe conv-param .\Game\Resource\Assets\Paramater .\Game\Resource\Orginal\Paramater\Game.xlsm
+Tool\GameDataMaker\20241121_180832\main.exe conv-param .\Game\Resource\TestAssets\Paramater .\Game\Resource\Orginal\Paramater\Test.xlsm
 
-rem アセットファイルを読み込みディレクトリに転送
+REM アセットファイルを読み込みディレクトリに転送
 call .\HobbyEngine\Script\MoveDir.bat .\Game\Resource\Assets\ .\Bin\SuperStar\Debug\Assets
-call .\HobbyEngine\Script\MoveDir.bat .\Game\Resource\Assets\ .\Bin\SuperStar\Release\Assets
+if %errorlevel% neq 0 pause & exit /b %errorlevel%
 
-call .\HobbyEngine\Script\MoveDir.bat .\Game\Resource\Assets\ .\Bin\Test\Debug\Assets
-pause & exit /b %errorlevel%
+call .\HobbyEngine\Script\MoveDir.bat .\Game\Resource\Assets\ .\Bin\SuperStar\Release\Assets
+if %errorlevel% neq 0 pause & exit /b %errorlevel%
+
+call .\HobbyEngine\Script\MoveDir.bat .\Game\Resource\TestAssets\ .\Bin\Test\Debug\Assets
+if %errorlevel% neq 0 pause & exit /b %errorlevel%
 
 endlocal
+
+REM 正常終了
+echo "All Complete" & pause & exit /b 0
+
