1a289d9de5d25a0f596bc26bcdfb826d09becf1f
kuronokurujp <kuronokurujp@gmail.com>
2024-12-06 18:04:41 +0900

    [modify] 構築した開発プロジェクトを整理するバッチファイルの文字コードをUTF8 + エラー処理追加

--- SuperStar/ProjClean.bat
+++ SuperStar/ProjClean.bat
@@ -1,11 +1,31 @@
 @echo off
-rem プロジェクトのクリーン処理
+chcp 65001
+
+REM プロジェクトのクリーン処理
 
 setlocal
 
-rem  プロジェクトフォルダを削除
-rmdir /s /q Build\
-rem 実行ファイルを収めてフォルダを削除
-rmdir /s /q Bin\
+REM  プロジェクトフォルダを削除
+
+set BUILD_DIR="Build\"
+if exist %BUILD_DIR% (
+    rmdir /s /q %BUILD_DIR%
+    echo "delete build dir"
+) else (
+    echo "error delete build dir"
+)
+
+REM 実行ファイルを収めてフォルダを削除
+set BIN_DIR="Bin\"
+if exist %BIN_DIR% (
+    rmdir /s /q %BIN_DIR%
+    echo "delete bin dir"
+) else (
+    echo "error delete bin dir"
+)
 
 endlocal
+
+REM 正常終了
+echo "all complete" & pause & exit /b 0
+
