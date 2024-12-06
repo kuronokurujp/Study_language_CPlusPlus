2376d48228aa92023e33a65c4a79de2de0123a6e
kuronokurujp <kuronokurujp@gmail.com>
2024-12-06 18:02:49 +0900

    [modify] コードを自動整形するバッチファイルの文字コードをUTF8に変更

--- SuperStar/CodeCleanup.bat
+++ SuperStar/CodeCleanup.bat
@@ -1,10 +1,15 @@
 @echo off
+chcp 65001
 
-rem cpp/h/hppファイルのコード整理を一括で行うバッチファイル
+REM cpp/h/hppファイルのコード整理を一括で行うバッチファイル
 
 setlocal
 
+REM エンジンにあるコード整理を行うスクリプトを起動
 call .\HobbyEngine\Script\CodeCleanup\CodeCleanup.bat .\
 pause & exit /b %errorlevel%
 
 endlocal
+
+REM 正常終了
+echo "all complete" & pause & exit /b 0
