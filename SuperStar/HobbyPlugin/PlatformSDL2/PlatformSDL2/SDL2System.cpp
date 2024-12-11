02d66228a7a9de69ed3ec45e08a43ad5f8654ae4
kuronokurujp <kuronokurujp@gmail.com>
2024-12-11 17:21:15 +0900

    [modify] SDL2プラットフォーム用の乱数機能を追加

--- SuperStar/HobbyPlugin/PlatformSDL2/PlatformSDL2/SDL2System.cpp
+++ SuperStar/HobbyPlugin/PlatformSDL2/PlatformSDL2/SDL2System.cpp
@@ -1,14 +1,20 @@
 ﻿#include "SDL2System.h"
 
+#include <time.h>
+
 namespace PlatformSDL2
 {
     HE::Uint32 System::VGetRand(const HE::Uint32 in_uMax)
     {
-        return 0;
+        // 0からin_uMax間の値を取得
+        return static_cast<HE::Uint32>(rand() % (in_uMax + 1));
     }
 
     HE::Bool System::VSetSeedRand(const HE::Uint32 in_uVal)
     {
-        return FALSE;
+        // 乱数の種を初期化
+        srand(static_cast<unsigned int>(in_uVal));
+
+        return TRUE;
     }
 }  // namespace PlatformSDL2
