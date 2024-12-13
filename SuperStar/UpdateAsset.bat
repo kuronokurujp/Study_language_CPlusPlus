73536515605a0546fa6d4d53f10a548435e8e4e8
kuronokurujp <kuronokurujp@gmail.com>
2024-12-13 21:30:38 +0900

    [modify]デバッグ実行時に更新アセットバッチファイルを呼ぶようにVS2022プロジェクト生成処理を変更

--- SuperStar/UpdateAsset.bat
+++ SuperStar/UpdateAsset.bat
@@ -5,22 +5,24 @@ rem アセットデータを更新バッチ
 
 setlocal
 
+REM バッチファイル実行場所によって失敗しないようにパスをバッチファイル場所を基準にする
+set SCRIPT_PATH=%~dp0
+
 REM パラメータデータをコンバート
-Tool\GameDataMaker\20241121_180832\main.exe conv-param .\Game\Resource\Assets\Paramater .\Game\Resource\Orginal\Paramater\Game.xlsm
-Tool\GameDataMaker\20241121_180832\main.exe conv-param .\Game\Resource\TestAssets\Paramater .\Game\Resource\Orginal\Paramater\Test.xlsm
+%SCRIPT_PATH%\Tool\GameDataMaker\20241121_180832\main.exe conv-param %SCRIPT_PATH%\Game\Resource\Assets\Paramater %SCRIPT_PATH%\Game\Resource\Orginal\Paramater\Game.xlsm
+%SCRIPT_PATH%\Tool\GameDataMaker\20241121_180832\main.exe conv-param %SCRIPT_PATH%\Game\Resource\TestAssets\Paramater %SCRIPT_PATH%\Game\Resource\Orginal\Paramater\Test.xlsm
 
 REM アセットファイルを読み込みディレクトリに転送
-call .\HobbyEngine\Script\MoveDir.bat .\Game\Resource\Assets\ .\Bin\SuperStar\Debug\Assets
+call %SCRIPT_PATH%\HobbyEngine\Script\MoveDir.bat %SCRIPT_PATH%\Game\Resource\Assets\ %SCRIPT_PATH%\Bin\SuperStar\Debug\Assets
 if %errorlevel% neq 0 pause & exit /b %errorlevel%
 
-call .\HobbyEngine\Script\MoveDir.bat .\Game\Resource\Assets\ .\Bin\SuperStar\Release\Assets
+call %SCRIPT_PATH%\HobbyEngine\Script\MoveDir.bat %SCRIPT_PATH%\Game\Resource\Assets\ %SCRIPT_PATH%\Bin\SuperStar\Release\Assets
 if %errorlevel% neq 0 pause & exit /b %errorlevel%
 
-call .\HobbyEngine\Script\MoveDir.bat .\Game\Resource\TestAssets\ .\Bin\Test\Debug\Assets
+call %SCRIPT_PATH%\HobbyEngine\Script\MoveDir.bat %SCRIPT_PATH%\Game\Resource\TestAssets\ %SCRIPT_PATH%\Bin\Test\Debug\Assets
 if %errorlevel% neq 0 pause & exit /b %errorlevel%
 
 endlocal
 
 REM 正常終了
 echo "All Complete" & pause & exit /b 0
-
