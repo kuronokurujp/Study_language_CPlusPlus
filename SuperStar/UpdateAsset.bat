8a280b4775480ef56e7476fabdfb21a493a23389
kuronokurujp <kuronokurujp@gmail.com>
2024-12-20 18:59:38 +0900

    [add] フォントコンバートとそれを処理するバッチ処理を追加

--- SuperStar/UpdateAsset.bat
+++ SuperStar/UpdateAsset.bat
@@ -1,16 +1,23 @@
 @echo off
 chcp 65001
 
-rem アセットデータを更新バッチ
+REM アセットデータを更新バッチ
 
 setlocal
 
 REM バッチファイル実行場所によって失敗しないようにパスをバッチファイル場所を基準にする
 set SCRIPT_PATH=%~dp0
 
+REM リソースのパス
+set RES_PATH=%SCRIPT_PATH%\Game\Resource
+
 REM パラメータデータをコンバート
-%SCRIPT_PATH%\Tool\GameDataMaker\20241121_180832\main.exe conv-param %SCRIPT_PATH%\Game\Resource\Assets\Paramater %SCRIPT_PATH%\Game\Resource\Orginal\Paramater\Game.xlsm
-%SCRIPT_PATH%\Tool\GameDataMaker\20241121_180832\main.exe conv-param %SCRIPT_PATH%\Game\Resource\TestAssets\Paramater %SCRIPT_PATH%\Game\Resource\Orginal\Paramater\Test.xlsm
+%SCRIPT_PATH%\Tool\GameDataMaker\20241121_180832\main.exe conv-param %RES_PATH%\Assets\Paramater %RES_PATH%\Orginal\Paramater\Game.xlsm
+%SCRIPT_PATH%\Tool\GameDataMaker\20241121_180832\main.exe conv-param %RES_PATH%\TestAssets\Paramater %RES_PATH%\Orginal\Paramater\Test.xlsm
+
+REM ゲームで使うフォントを作成
+%SCRIPT_PATH%\Tool\FontConverter\20241214_173447\main.exe conv %RES_PATH%\Orginal\Font\FontConverterConfig.ini %RES_PATH%\TestAssets\Font %RES_PATH%\Orginal\Font\PixelMplus10-Bold.ttf TestFont.ttf
+%SCRIPT_PATH%\Tool\FontConverter\20241214_173447\main.exe conv %RES_PATH%\Orginal\Font\FontConverterConfig.ini %RES_PATH%\Assets\Font %RES_PATH%\Orginal\Font\PixelMplus10-Bold.ttf Font.ttf
 
 REM アセットファイルを読み込みディレクトリに転送
 call %SCRIPT_PATH%\HobbyEngine\Script\MoveDir.bat %SCRIPT_PATH%\Game\Resource\Assets\ %SCRIPT_PATH%\Bin\SuperStar\Debug\Assets
