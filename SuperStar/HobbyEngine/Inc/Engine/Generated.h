ac153659ef86285f00f7629b763895dc4ffba29b
kuronokurujp <kuronokurujp@gmail.com>
2025-06-21 22:10:03 +0900

    [fixbug] RTTIの静的変数と関数内で行うとモジュール間で使用した場合にリンクエラーになるとのことなのでクラスの静的変数に変えてみた

--- SuperStar/HobbyEngine/Inc/Engine/Generated.h
+++ SuperStar/HobbyEngine/Inc/Engine/Generated.h
@@ -4,36 +4,36 @@
 
 // RTTIを付与する基本クラスの宣言にRTTI情報を付与するマクロ
 // nameにはクラス型を指定
-#define HE_GENERATED_CLASS_BASE_BODY_HEADER(name)                                             \
-public:                                                                                       \
-    static const Core::Common::RTTI* StaticRTTI()                                             \
-    {                                                                                         \
-        static Core::Common::RTTI name##_CLASS_RTTI = Core::Common::RTTI(HE_STR_TEXT(#name)); \
-                                                                                              \
-        return &name##_CLASS_RTTI;                                                            \
-    }                                                                                         \
-                                                                                              \
-public:                                                                                       \
-    const Core::Common::RTTI& GetRTTI()                                                       \
-    {                                                                                         \
-        return *(StaticRTTI());                                                               \
+#define HE_GENERATED_CLASS_BASE_BODY_HEADER(name)              \
+public:                                                        \
+    static inline const Core::Common::RTTI name##_CLASS_RTTI = \
+        Core::Common::RTTI(HE_STR_TEXT(#name));                \
+    static const Core::Common::RTTI* StaticRTTI()              \
+    {                                                          \
+        return &name##_CLASS_RTTI;                             \
+    }                                                          \
+                                                               \
+public:                                                        \
+    const Core::Common::RTTI& GetRTTI()                        \
+    {                                                          \
+        return *(StaticRTTI());                                \
     }
 
 // RTTIを付与した基本クラスを継承したクラスの宣言にRTTI情報を付与するマクロ
 // nameにはクラス型を指定
-#define HE_GENERATED_CLASS_BODY_HEADER(name, parentName)                         \
-public:                                                                          \
-    static const Core::Common::RTTI* StaticRTTI()                                \
-    {                                                                            \
-        static Core::Common::RTTI name##_CLASS_RTTI =                            \
-            Core::Common::RTTI(HE_STR_TEXT(#name), parentName## ::StaticRTTI()); \
-        return &name##_CLASS_RTTI;                                               \
-    }                                                                            \
-                                                                                 \
-public:                                                                          \
-    const Core::Common::RTTI& GetRTTI()                                          \
-    {                                                                            \
-        return *(StaticRTTI());                                                  \
+#define HE_GENERATED_CLASS_BODY_HEADER(name, parentName)                     \
+public:                                                                      \
+    static inline const Core::Common::RTTI name##_CLASS_RTTI =               \
+        Core::Common::RTTI(HE_STR_TEXT(#name), parentName## ::StaticRTTI()); \
+    static const Core::Common::RTTI* StaticRTTI()                            \
+    {                                                                        \
+        return &name##_CLASS_RTTI;                                           \
+    }                                                                        \
+                                                                             \
+public:                                                                      \
+    const Core::Common::RTTI& GetRTTI()                                      \
+    {                                                                        \
+        return *(StaticRTTI());                                              \
     }
 
 #define HE_GENERATED_CHECK_RTTI(rtti, checkRtti) \
