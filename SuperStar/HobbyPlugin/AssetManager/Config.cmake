1ff3d349bef3fa7806b002e1919da9af78aff670
kuronokurujp <kuronokurujp@gmail.com>
2025-06-24 22:55:56 +0900

    [modify] AssetManagerのプロジェクトを再構築

--- SuperStar/HobbyPlugin/AssetManager/Config.cmake
+++ SuperStar/HobbyPlugin/AssetManager/Config.cmake
@@ -1,32 +1,39 @@
-set(ASSET_MANAGER_PLUGIN_NAME HobbyEnginePlugin_AssetManager)
+set(PLUGIN_ASSET_MANAGER_NAME HobbyPlugin_AssetManager)
+set(PLUGIN_ASSET_MANAGER_INC_DIR ${CMAKE_CURRENT_LIST_DIR}/Inc)
 
-# プロジェクトに登録するファイル群
-# ここに全て記載する
-set(ASSET_MANAGER_PLUGIN_TARGET_FILES
+set(PLUGIN_ASSET_MANAGER_TARGET_COMMON_FILES
     .editorconfig
     LICENSE.txt
     README.md
+)
 
-    AssetManagerModule.cpp
-    AssetManagerModule.h
-    
-    AssetManager/AssetDataBase.h
-    AssetManager/AssetDataBase.cpp
-    AssetManager/AssetDataJson.h
-    AssetManager/AssetDataJson.cpp
-    AssetManager/AssetDataToml.h
-    AssetManager/AssetDataToml.cpp
-    AssetManager/AssetDataXml.h
-    AssetManager/AssetDataXml.cpp
-    AssetManager/AssetDataBinary.h 
-    AssetManager/AssetDataBinary.cpp
-    AssetManager/AssetDataText.h 
-    AssetManager/AssetDataText.cpp
+set(PLUGIN_ASSET_MANAGER_TARGET_INC_FILES
+    Inc/AssetManagerModule.h
 
-    AssetManager/ThirdParty/pugixml/pugiconfig.hpp
-    AssetManager/ThirdParty/pugixml/pugixml.cpp
-    AssetManager/ThirdParty/pugixml/pugixml.hpp
-    AssetManager/ThirdParty/simidjson/simdjson.h
-    AssetManager/ThirdParty/simidjson/simdjson.cpp
-    AssetManager/ThirdParty/tomlplusplus/toml.hpp
+    Inc/AssetManager/AssetDataBase.h
+    Inc/AssetManager/AssetDataJson.h
+    Inc/AssetManager/AssetDataToml.h
+    Inc/AssetManager/AssetDataXml.h
+    Inc/AssetManager/AssetDataBinary.h
+    Inc/AssetManager/AssetDataText.h
+)
+
+
+set(PLUGIN_ASSET_MANAGER_TARGET_SRC_FILES
+    Src/AssetManagerModule.cpp
+
+    Src/AssetDataBase.cpp
+    Src/AssetDataJson.cpp
+    Src/AssetDataToml.cpp
+    Src/AssetDataXml.cpp
+    Src/AssetDataBinary.cpp
+    Src/AssetDataText.cpp
+
+    # ThirdPartyの機能は外部公開したくないのでSRCファイルリストに
+    ThirdParty/pugixml/pugiconfig.hpp
+    ThirdParty/pugixml/pugixml.cpp
+    ThirdParty/pugixml/pugixml.hpp
+    ThirdParty/simidjson/simdjson.h
+    ThirdParty/simidjson/simdjson.cpp
+    ThirdParty/tomlplusplus/toml.hpp
 )
---
+++ SuperStar/HobbyPlugin/AssetManager/Test/Config.cmake
@@ -0,0 +1,6 @@
+set(PLUGIN_ASSET_MANGER_TEST_NAME HobbyPlugin_Actor_UnitTests)
+
+# テストソースをまとめる（複数ファイルがあればここに追加）
+set(PLUGIN_ASSET_MANGER_TARGET_TEST_SRC_FILES
+    AssetManager/TestAssetManager.cpp
+)
