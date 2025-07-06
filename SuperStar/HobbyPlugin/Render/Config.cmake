d83dd6770096c7026b28dd3a65ce43992232005f
kuronokurujp <kuronokurujp@gmail.com>
2025-07-06 20:31:18 +0900

    [modify / del] レンダリングエンジンの初期化作業に抜けがあったので追加対応

--- SuperStar/HobbyPlugin/Render/Config.cmake
+++ SuperStar/HobbyPlugin/Render/Config.cmake
@@ -7,20 +7,6 @@ set(RENDER_PLUGIN_TARGET_FILES
     LICENSE.txt
     README.md
 
-    RenderModule.cpp
-    RenderModule.h
-    
-    Render/Particle/Blob.cpp
-    Render/Particle/Blob.h
-
-    Render/Command/CommandBuffer.h
-    Render/Command/Command.h
-    Render/Command/Command.cpp
-
-    Render/Window/Window.cpp
-    Render/Window/Window.h
-    Render/Window/ViewPort.h
-    Render/Window/ViewPort.cpp
-    Render/Window/Scene.h
-    Render/Window/Scene.cpp
+    Src/RenderModule.cpp
+    Inc/RenderModule.h
 )
