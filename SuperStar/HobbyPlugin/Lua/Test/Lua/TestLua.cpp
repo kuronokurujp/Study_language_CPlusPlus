8cd298f955c6fb779000a2b4b734309842c31f56
Kuronokurujp <kuronokurujp@gmail.com>
2025-07-06 15:23:03 +0900

    Add Lua module unit test

--- SuperStar/HobbyPlugin/Lua/Test/Lua/TestLua.cpp
+++ SuperStar/HobbyPlugin/Lua/Test/Lua/TestLua.cpp
@@ -1,11 +1,53 @@
-﻿#include <gtest/gtest.h>
+#include <gtest/gtest.h>
 
 #include "LuaModule.h"
 
 namespace Lua
 {
-    TEST(HobbyPlugin_Lua, Text)
+    TEST(HobbyPlugin_Lua, CallScript)
     {
+        auto spLuaModule = HE_MAKE_CUSTOM_SHARED_PTR((Lua::LuaModule));
+
+        Module::ModuleManager moduleManager;
+        EXPECT_EQ(moduleManager.AddModule(spLuaModule), TRUE);
+        EXPECT_EQ(moduleManager.Start(), TRUE);
+
+        auto luaHandle = spLuaModule->CreateLuaObject(HE_STR_TEXT("Test"));
+        EXPECT_EQ(luaHandle.Null(), FALSE);
+
+        EXPECT_EQ(spLuaModule->RegistScriptFunc(luaHandle, HE_STR_TEXT("NativeFunc")), TRUE);
+
+        bool       called = false;
+        LuaFuncData result{};
+        {
+            auto upFunc = HE_MAKE_CUSTOM_UNIQUE_PTR((Core::Common::FunctionObject<void, LuaFuncData&>),
+                                                   [&called, &result](LuaFuncData& d) {
+                                                       called = true;
+                                                       result = d;
+                                                   });
+            spLuaModule->AddListenerLuaFunc(std::move(upFunc));
+        }
+
+        const HE::Char* script = HE_STR_TEXT(
+            "function LuaEntry()\n"
+            "    NativeFunc(11, \"hello\")\n"
+            "end\n");
+
+        EXPECT_EQ(spLuaModule->LoadScriptText(luaHandle, script), TRUE);
+        EXPECT_EQ(spLuaModule->CallScriptFunc(luaHandle, HE_STR_TEXT("LuaEntry")), TRUE);
+
+        moduleManager.LateUpdate(0.0f);
+
+        EXPECT_EQ(called, true);
+        EXPECT_EQ(HE_STR_CMP(result.szFuncName, HE_STR_TEXT("NativeFunc")), 0);
+        EXPECT_EQ(result.uArgCount, 2u);
+        EXPECT_EQ(result.aArg[0].eValType, Lua::ELuaFuncArgType_Uint64);
+        EXPECT_EQ(result.aArg[0]._data.uuVal, 11u);
+        EXPECT_EQ(result.aArg[1].eValType, Lua::ELuaFuncArgType_Str);
+        EXPECT_EQ(HE_STR_CMP(result.aArg[1]._data.szText, HE_STR_TEXT("hello")), 0);
+
+        EXPECT_EQ(spLuaModule->ReleaseLuaObject(luaHandle), TRUE);
+        EXPECT_EQ(moduleManager.Release(), TRUE);
     }
 }  // namespace Lua
 
